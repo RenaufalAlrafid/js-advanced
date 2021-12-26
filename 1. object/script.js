54ec9512067a3fa667c786699d2640cc2b34805c
RenaufalAlrafid <renaufalalrafid@mail.ugm.ac.id>
2021-12-26 16:50:25 +0700

    onject 1.1

---
+++ 1. object/script.js
@@ -0,0 +1,65 @@
+// cara membuat object pada java script
+
+//* object literal (untuk satu object/ tidak efektif untuk object banyak )
+
+let mahasiswa1 = {
+    nama: 'Renaufal',
+    energi : 10,
+    makan: function(porsi){
+        this.energi= this.energi + porsi;
+        console.log(`Halo ${this.nama}, Selamat Makan`)
+    }
+}
+let mahasiswa2 = {
+    nama: 'Alrafid',
+    energi : 20,
+    makan: function(porsi){
+        this.energi= this.energi + porsi;
+        console.log(`Halo ${this.nama}, Selamat Makan`)
+    }
+};
+
+
+//* function declaration ()
+
+function mahasiswa(nama,energi){
+    let mahasiswa = {};
+        mahasiswa.nama = nama;
+        mahasiswa.energi = energi;
+
+        mahasiswa.makan = function(porsi){
+            this.energi += porsi;
+            console.log(`Halo ${this.nama}, Selamat Makan`)
+        };
+
+        mahasiswa.main = function(jam){
+            this.energi -= jam;
+        }
+        
+    return mahasiswa;
+}
+
+// let renaufal = mahasiswa('Renaufal' , 10);
+// let doddy = mahasiswa('doddy', 20);
+
+//* constractor function
+function mahasiswa(nama,energi){
+        this.nama = nama;
+        this.energi = energi;
+
+        this.makan = function(porsi){
+            this.energi += porsi;
+            console.log(`Halo ${this.nama}, Selamat Makan`)
+        };
+
+        this.main = function(jam){
+            this.energi -= jam;
+        }
+}
+
+
+let renaufal = new mahasiswa('Renaufal' , 10);
+let doddy = new mahasiswa('doddy', 20);
+
+
+//* object.create()
