6325071aa43bea50f1841e75774dbada346cfba1
RenaufalAlrafid <renaufalalrafid@mail.ugm.ac.id>
2021-12-27 11:40:16 +0700

    menambah study kasus

--- 3. arrow function/script.js
+++ 3. arrow function/script.js
@@ -1,4 +1,4 @@
-//* arrow function
+//* arrow function (gak punya this)
 
 // const tampilNama = (nama) => {return `halo, ${nama}`;}
 // console.log(tampilNama("renaufal"));
@@ -35,3 +35,71 @@
 
 //* this pada arrow function
 
+// constractor function
+
+
+// const mahasiswa = function(){
+//     this.nama = 'Renaufal';
+//     this.umur = 22;
+//     this.sayHello = function(){
+//         console.log(`Halo Nama Saya ${this.nama} dan umur saya ${this.umur}`);
+//     }
+// }
+
+// const Renaufal = new mahasiswa();
+
+// arrow function
+// const mahasiswa = function(){
+//     this.nama = 'Renaufal';
+//     this.umur = 22;
+//     this.sayHello = () => {
+//         console.log(`Halo Nama Saya ${this.nama} dan umur saya ${this.umur}`);
+//     }
+// }
+
+// const Renaufal = new mahasiswa();
+
+
+
+// object literal 
+
+// const mhs1 ={
+//     nama: "renauufa",
+//     umur: 22,
+//     sayHello: () => {
+//         console.log(`Halo Nama Saya ${mhs1.nama} dan umur saya ${mhs1.umur}`);
+//     }
+// }
+
+
+
+// const mahasiswa = function(){
+//     this.nama = 'Renaufal';
+//     this.umur = 22;
+//     this.sayHello = function(){
+//         console.log(`Halo Nama Saya ${this.nama} dan umur saya ${this.umur}`);
+//     }
+
+//     setInterval(() => {
+//         console.log(this.umur++);
+//     },500);
+// }
+
+// const Renaufal = new mahasiswa();
+
+
+// study kasus
+
+
+const box = document.querySelector('.box');
+box.addEventListener('click', function(){
+    let satu = 'size';
+    let dua = 'caption';
+    if(this.classList.contains(satu)){
+        [satu, dua] = [dua, satu];
+    }
+    this.classList.toggle(satu);
+    setTimeout(()=>{
+        this.classList.toggle(dua);
+    },600)
+});
