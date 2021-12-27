ebf4b9971932520c54ae0e73cdfdc573f31d1768
RenaufalAlrafid <renaufalalrafid@mail.ugm.ac.id>
2021-12-27 10:16:37 +0700

    arrow funvtion

---
+++ 3. arrow function/script.js
@@ -0,0 +1,37 @@
+//* arrow function
+
+// const tampilNama = (nama) => {return `halo, ${nama}`;}
+// console.log(tampilNama("renaufal"));
+
+
+// const tampilNama = (nama,waktu) => {
+//     return `Selamat ${waktu}, ${nama}`;
+// }
+
+// console.log(tampilNama("Renaufal", "Siang"));
+
+// const tampilNama = nama => `halo, ${nama}`;
+// console.log(tampilNama("renaufal"));
+
+// const tampilNama = () => 'Hello World';
+
+// let mahasiswa = ["Renaufal" , "Yolo", "gess"];
+
+// let jumlahHuruf = mahasiswa.map(function(nama){
+//     return nama.length;
+// })
+
+// console.log(jumlahHuruf);
+
+
+// let jumlahHuruf = mahasiswa.map(nama => nama.length)
+
+// console.log(jumlahHuruf);
+
+// let jumlahHuruf = mahasiswa.map(nama => ({nama, jmlHuruf: nama.length}));
+
+// console.table(jumlahHuruf);
+
+
+//* this pada arrow function
+
