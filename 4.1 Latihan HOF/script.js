4112889b31239c9fda76a4331a9af4a19e87da3b
RenaufalAlrafid <renaufalalrafid@mail.ugm.ac.id>
2021-12-28 13:21:43 +0700

    latihan dan template literals

--- 4.1 Latihan HOF/script.js
+++ 4.1 Latihan HOF/script.js
@@ -3,15 +3,33 @@
 const videos = Array.from(document.querySelectorAll('[data-duration]'));
 
 // pilih yang js lanjutan
-const lanjut = videos.filter(video => video.textContent.includes('JAVASCRIPT LANJUTAN'));
+const lanjut = videos.filter(video => video.textContent.includes('JAVASCRIPT LANJUTAN'))
 
 // ambil durasi
+.map(item=> item.dataset.duration)
 
 // ubah ke integer , ubah menjadi detik
+.map(waktu =>{
+    const parts = waktu.split(':').map(part => parseFloat(part));
+    return (parts[0]* 60) + parts[1];
+})
 
 // jumlahkan semua detik
-
+.reduce((total, detik)=> total + detik);
 
 // ubah formatnya jadi jam menit detik
 
+const jam = Math.floor(lanjut / 3600);
+const menit = Math.floor((lanjut - (jam*3600)) /60 );
+const detik = lanjut - (jam*3600) - (menit*60);
+
+
 // simpan didom
+const pDurasi = document.querySelector('.total-durasi');
+pDurasi.textContent = `${jam} jam, ${menit} menit, ${detik} detik`;
+const jmlVideo = videos.filter(video => video.textContent.includes('JAVASCRIPT LANJUTAN')).length;
+const pVideo = document.querySelector('.jumlah-video');
+pVideo.textContent = `${jmlVideo} video`;
+
+
+console.log(detik);
---
+++ 5. tamplate literal/script.js
@@ -0,0 +1,30 @@
+// tamplate literal 
+
+//* tamplate string
+// const nama = 'Renaufal AI';
+// const umur = 22;
+// console.log(`Halo nama saya ${nama}, umur saya ${umur}`);
+
+//* embeded expresion
+// console.log(`${alert('halo')}`);
+
+// const x = 10;
+// console.log(`${(x%2 == 0) ? 'genap' : 'ganjil'}`);
+
+
+//HTML fragment 
+
+// const mhs = {
+//     nama: 'renaufal',
+//     umur: 22,
+//     nim: '409417',
+//     email: 'renaufal11@gmail.com'
+// };
+
+
+// const el = `<div class="mhs">
+//     <h2>${mhs.nama}</h2>
+//     <span class="nrp">${mhs.nim}</span>
+// </div>`;
+
+// console.log(el);
