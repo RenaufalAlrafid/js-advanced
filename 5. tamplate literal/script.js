52076ed3c6344c4cff58cec49e90b50aa0972219
RenaufalAlrafid <renaufalalrafid@mail.ugm.ac.id>
2021-12-28 21:55:26 +0700

    tagged tamplate literals

--- 5. tamplate literal/script.js
+++ 5. tamplate literal/script.js
@@ -74,33 +74,69 @@
 
 //* nested
 
-const mhs = {
-            nama: 'renaufal',
-            semester: 5,
-            matkul: [
-                'rekayasa web',
-                'analisis metode geofisika',
-                'pemrogaman geofisika',
-                'metode seismik'
-            ]
-        };
+// const mhs = {
+//             nama: 'renaufal',
+//             semester: 5,
+//             matkul: [
+//                 'rekayasa web',
+//                 'analisis metode geofisika',
+//                 'pemrogaman geofisika',
+//                 'metode seismik'
+//             ]
+//         };
 
-function cetakMataKuliah(matkul) {
-    return `
-    <ol class="">
-    ${matkul.map(mk=>`<li class="">${mk}</li>`).join('')}
-    </ol>
-    `;
+// function cetakMataKuliah(matkul) {
+//     return `
+//     <ol class="">
+//     ${matkul.map(mk=>`<li class="">${mk}</li>`).join('')}
+//     </ol>
+//     `;
+// }
+
+
+// const el = `<div class="mhs">
+//     <h2>nama :${mhs.nama}</h2>
+//     <span class="semester"> Semester : ${mhs.semester}</span>
+//     <h4 class="">mata kuliah</h4>
+//     ${cetakMataKuliah(mhs.matkul)}
+// </div>`;
+
+
+
+// document.body.innerHTML = el;
+
+
+
+//* tagged tamplates
+// const nama = 'renaufal';
+// const umur = 22;
+
+// function coba(strings, ...values) {
+//     // let result ='';
+//     // strings.forEach((str, i) => {
+//     //     result += `${str}${values[i] || ''}`; 
+//     // });
+//     // return result;
+
+
+//     return strings.reduce((result, str, i) => `${result}${str}${values[i] || ''}`, '' )
+// }
+
+// const str = coba`Halo, nama saya ${nama}, saya ${umur} tahun.`;
+// console.log(str);
+
+
+// higlighy
+
+const nama = 'renaufal';
+const umur = 22;
+const email = 'renaufal@gmail.com'
+
+function highlight(strings, ...values) {
+
+    return strings.reduce((result, str, i) => `${result}${str}<span class="hl">${values[i] || ''}</span>`, '' )
 }
 
+const str = highlight`Halo, nama saya ${nama}, saya ${umur} tahun. dan email saya adalah ${email}`;
+document.body.innerHTML = str;
 
-const el = `<div class="mhs">
-    <h2>nama :${mhs.nama}</h2>
-    <span class="semester"> Semester :${mhs.semester}</span>
-    <h4 class="">mata kuliah</h4>
-    ${cetakMataKuliah(mhs.matkul)}
-</div>`;
-
-
-
-document.body.innerHTML = el;
